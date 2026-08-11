989ee0eedc0d9b358d827157eefe38076c8483f5
AkiharaYanagi <150650129+AkiharaYanagi@users.noreply.github.com>
2026-08-11 10:49:28 +0900

    e

--- 03_FtgMain/Source/ExeChara/Disp/DispMainImage.cpp
+++ 03_FtgMain/Source/ExeChara/Disp/DispMainImage.cpp
@@ -97,7 +97,9 @@ namespace GAME
 		//VEC2 vecImg = VEC2 ( x, y );
 
 		//整数化しておく　テクスチャの描画位置は整数でないとアトラスのスキマが見える
-		VEC2 vecImg = VEC2 ( static_cast < int > ( x ), static_cast < int > ( y ) );
+		int fx = static_cast < int > ( x );
+		int fy = static_cast < int > ( y );
+		VEC2 vecImg = VEC2 ( fx, fy  );
 
 		//IDチェックはスクリプト作成時、ロード時に行う
 		UINT index = pScript->ImageIndex.Get();
